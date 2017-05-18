49434b3e4c1097e856e3121dfb0fe352eb5d5d65
KeijiBranshi <bkhorton@usc.edu>
2017-05-17 19:42:38 -0700

    demos up and running

---
+++ UnityDataVisualization/Assets/Scripts/Control/ControllableChildObject.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CACS
+{
+  public class ControllableChildObject : ControllableObject
+  {
+
+    List<Material> materials;
+
+    // Use this for initialization
+    void Start() {
+      Material[] mats = GetComponent<MeshRenderer>().sharedMaterials;
+      materials = new List<Material>();
+
+      foreach (Material mat in gameObject.GetComponent<MeshRenderer>().sharedMaterials) {
+        materials.Add(mat);
+      }
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    /* -----------------------------------------------------------
+     * Trigger/Collision Detection
+     * -----------------------------------------------------------*/
+    void OnTriggerStay(Collider col) {
+      Vector3 normal;
+      Vector3 position;
+      float viewingDepth;
+
+      normal = col.gameObject.transform.up;
+      position = col.gameObject.transform.position;
+      viewingDepth = col.GetComponent<ControllablePlane>().getViewingDepth();
+
+      setShaderParams(normal, position, viewingDepth);
+    }
+
+    void OnTriggerExit(Collider col) {
+      setShaderParams(new Vector4(0, 0, 0, 0), new Vector4(0, 0, 0, 0), 0);
+    }
+
+    private void setShaderParams(Vector3 normal, Vector3 pos, float viewingDepth) {
+      foreach (Material mat in materials) {
+        mat.SetVector("_Normal", normal);
+        mat.SetVector("_PlanePosition", pos);
+        mat.SetFloat("_Distance", viewingDepth);
+      }
+    }
+    /*------------------------------------------------------------*/
+  }
+
+}
---
+++ UnityDataVisualization/Assets/Scripts/Control/XBoxController.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CACS
+{
+  public class XBoxController : Controller
+  {
+    // XBox Controller Specific Inputs
+    private float scrollchange = 0;
+    private float left_horiz = 0;
+    private float right_vert = 0;
+    private float right_horiz = 0;
+    private float left_trig = 0;
+    private float right_trig = 0;
+
+    // To account for loose/used joysticks
+    private float looseJoystickParam = 0.2f;
+
+    // Use this for initialization
+    new void Start() {
+      base.Start();
+    }
+
+    // Update is called once per frame
+    void Update() {
+      getInput();
+            //processInput(objects[0]);
+            //processInput(plane);
+            processInput();
+      resetInput();
+            if (Input.GetButton("RightBumper"))
+            {
+                slider.value = slider.value + 0.01f;
+                Debug.Log("You pressed the right bumper");
+            } else if (Input.GetButton("LeftBumper"))
+            {
+                slider.value = slider.value - 0.01f;
+                Debug.Log("You pressed the left bumper");
+            }
+    }
+
+    /* -------------------------------------------------------------------------------
+     * Input Handling
+     * -------------------------------------------------------------------------------*/
+    protected override void getInput() {
+      //Get Controller Inputs
+      scrollchange = Input.GetAxis("LeftJoyVert");
+      left_horiz = Input.GetAxis("LeftJoyHoriz");
+
+      right_vert = Input.GetAxis("RightJoyVert");
+      right_horiz = Input.GetAxis("RightJoyHoriz");
+
+      left_trig = Input.GetAxis("LeftTrigger");
+      right_trig = Input.GetAxis("RightTrigger");
+    }
+
+    protected override void resetInput() {
+      scrollchange = 0f;
+      left_horiz = 0f;
+
+      right_vert = 0f;
+      right_horiz = 0f;
+
+      left_trig = 0f;
+      right_trig = 0f;
+    }
+
+    protected override void processInput(Controllable puppet) {
+      if (puppet != null) {
+        if (Math.Abs(right_horiz) > looseJoystickParam || Math.Abs(right_vert) > looseJoystickParam) {
+          puppet.rotate(right_horiz, right_vert, rotationSensitivity);
+        }
+        if (Math.Abs(scrollchange) > looseJoystickParam) {
+          puppet.scaleSize(scrollchange * zoomSensitivity);
+        }
+      }
+    }
+
+        protected void processInput()
+        {
+            if (Math.Abs(left_horiz) > looseJoystickParam)
+            {
+                transform.RotateAround(transform.position, Vector3.up, left_horiz);
+            }
+        }
+
+        private void deltaTimeInputs() {
+      scrollchange = scrollchange * Time.deltaTime;
+      left_horiz *= Time.deltaTime;
+
+      right_vert *= Time.deltaTime;
+      right_horiz *= Time.deltaTime;
+
+      left_trig *= Time.deltaTime;
+      right_trig *= Time.deltaTime;
+    }
+    /* -------------------------------------------------------------------------------*/
+  }
+
+}
