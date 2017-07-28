cb4d0e6900b697bb6c1f71665b7b942f90f22176
KeijiBranshi <bkhorton@usc.edu>
2017-07-28 10:15:43 -0700

    Readded Unity project

---
+++ UnityGEARS/Assets/Scripts/Control/OculusRemote.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OculusRemote : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        OVRInput.Update();
+		if (OVRInput.Get(OVRInput.Button.DpadLeft))
+        {
+            SceneManager.LoadSceneAsync("TellurineFun", LoadSceneMode.Single);
+        } else if (OVRInput.Get(OVRInput.Button.DpadRight))
+        {
+            SceneManager.LoadSceneAsync("ExfoliationFun", LoadSceneMode.Single);
+        }
+	}
+}
