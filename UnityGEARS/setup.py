761e65559598cd57f2708ef30df1bfb9a0973f1c
Brandon Horton <bkhorton@usc.edu>
2017-08-26 23:58:46 -0700

    Shrunk contact lens, about to reimport giant ass exfoliaton object

--- UnityGEARS/setup.py
+++ UnityGEARS/setup.py
@@ -12,7 +12,6 @@ currentDir = os.path.dirname(os.path.realpath(__file__))
 
 # Parse Arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("--interactive", help="Adds Interactive Data Viewer demo to the build", action="store_true")
 parser.add_argument("--vcm", help="Adds the Virtual Confocal Microscopy demo to the build", action="store_true")
 
 args = parser.parse_args();
@@ -29,9 +28,7 @@ geometryZip = 'GeometryData.zip'
 leapZipPath =  assetsPath + 'Leap_Motion_CoreAssets_4.2.1.zip'
 leapDir = assetsPath + 'LeapMotion/'
 leapMeta = assetsPath + 'LeapMotion.meta'
-demo1Dir = assetsPath + 'Demo1-DataViewer/'
 demo2Dir = assetsPath + 'Demo2-VirtualConfocalMicroscopy/'
-demo1GeometryZipPath = demo1Dir + geometryZip
 demo2GeometryZipPath = demo2Dir + geometryZip
 
 if not os.path.isdir(leapDir) and not os.path.isfile(leapMeta):
@@ -39,15 +36,6 @@ if not os.path.isdir(leapDir) and not os.path.isfile(leapMeta):
 	leapZip = zipfile.ZipFile(leapZipPath, 'r')
 	leapZip.extractall(assetsPath)
 	leapZip.close()
-
-if args.interactive:
-    if not os.path.isdir(demo1Dir + geometryDir) and not os.path.isfile(demo1Dir + geometryMeta):
-        print('Unzipping Interactive geometry data')
-        demo1Zip = zipfile.ZipFile(demo1GeometryZipPath, 'r')
-        demo1Zip.extractall(demo1Dir)
-        demo1Zip.close()
-    else:
-        print('Demo 1 Geometry Data already exists. If prefab errors occur, remove folder and meta files, then retry.')
 
 if args.vcm:
     if not os.path.isdir(demo2Dir + geometryDir) and not os.path.isfile(demo2Dir + geometryMeta):
@@ -60,7 +48,7 @@ if args.vcm:
 
 
 # Open Unity
-
+print('Startin Up Unity')
 subprocess.call(['C:/Program Files/Unity/Editor/Unity.exe', '-projectPath', fullProjectPath])
 
 print('Setup Complete')
