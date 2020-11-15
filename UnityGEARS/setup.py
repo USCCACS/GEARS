83a14450d0928ea4cec160b9a433eedc11c68d5d
Brandon Horton <brandon.keiji@gmail.com>
2020-11-14 16:59:50 -0800

    move exfoliation obj to gdrive (#1) (#7)
    * updated setup.py and cleaned up README
    
    * removed VCM zip file

--- UnityGEARS/setup.py
+++ UnityGEARS/setup.py
@@ -6,13 +6,44 @@ import os
 import os.path
 import subprocess
 import sys
+import requests
+
+# Credit where credit is due: https://stackoverflow.com/a/39225039
+def download_file_from_google_drive(id, destination):
+    def get_confirm_token(response):
+        for key, value in response.cookies.items():
+            if key.startswith('download_warning'):
+                return value
+
+        return None
+
+    def save_response_content(response, destination):
+        CHUNK_SIZE = 32768
+
+        with open(destination, "wb") as f:
+            for chunk in response.iter_content(CHUNK_SIZE):
+                if chunk: # filter out keep-alive new chunks
+                    f.write(chunk)
+
+    URL = "https://docs.google.com/uc?export=download"
+
+    session = requests.Session()
+
+    response = session.get(URL, params = { 'id' : id }, stream = True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = { 'id' : id, 'confirm' : token }
+        response = session.get(URL, params = params, stream = True)
+
+    save_response_content(response, destination)    
 
 #Acquire the current working directory
 currentDir = os.path.dirname(os.path.realpath(__file__))
 
 # Parse Arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("--vcm", help="Adds the Virtual Confocal Microscopy demo to the build", action="store_true")
+parser.add_argument("--noVCM", help="Skips setup for the Virtual Confocal Microscopy (Demo 2)", action="store_true")
 
 args = parser.parse_args();
 
@@ -37,18 +68,15 @@ if not os.path.isdir(leapDir) and not os.path.isfile(leapMeta):
 	leapZip.extractall(assetsPath)
 	leapZip.close()
 
-if args.vcm:
+if not args.noVCM:
     if not os.path.isdir(demo2Dir + geometryDir) and not os.path.isfile(demo2Dir + geometryMeta):
         print('Unzipping Virtual Confocal Microscopy geometry data')
+        geometryDataFileId = '1E0kapjSMS-4Wl6bqg2ERAD6R8xWKcxtx' # hard coded file id
+        download_file_from_google_drive(geometryDataFileId, demo2GeometryZipPath)
         demo2Zip = zipfile.ZipFile(demo2GeometryZipPath, 'r')
         demo2Zip.extractall(demo2Dir)
         demo2Zip.close()
     else:
-        print('Demo 2 Geometry Data already exists. If prefab errors occur, remove folder and meta files, then retry.')
-
-
-# Open Unity
-print('Startin Up Unity')
-subprocess.call(['C:/Program Files/Unity/Editor/Unity.exe', '-projectPath', fullProjectPath])
+        print('Demo 2 Geometry Data already exists. If prefab errors occur, remove folder and meta files, then retry.') 
 
 print('Setup Complete')
